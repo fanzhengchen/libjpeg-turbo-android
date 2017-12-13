64d23e2e37de739c25b09ad5fd1f006eb5f35b7d
fanzhengchen <fanzhengchen@gmail.com>
2017-12-13 11:46:03 +0800

    log debug

--- app/src/main/cpp/imageproc.h
+++ app/src/main/cpp/imageproc.h
@@ -14,7 +14,7 @@
 #define LOGW(...)  __android_log_write(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
-
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
 typedef u_int8_t BYTE;
 
 
